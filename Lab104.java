67d67f7426f52d25754274e8f08b40eb099cf36e
Thanachot Jatiradook <jatiradook13@gmail.com>
2025-11-21 18:18:35 +0700

    lab10401

--- Lab104.java
+++ Lab104.java
@@ -2,36 +2,23 @@ import java.util.Scanner;
 
 public class Lab104 {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Enter your score : ");
-
         int score = scanner.nextInt();
 
-        switch(score/10){
-
-            case 10:
-
-            case 8:
-                System.out.println("A");
-                break;
-
-            case 7:
+        if (score >= 80){
+            System.out.println("A");
+        }   else if (score >= 70){
                 System.out.println("B");
-                break;
-
-            case 6:
-                System.out.println("C");
-                break;
-
-            case 5:
+            }
+            else if (score >= 60){
+                System.out.println("c");
+            }
+            else if (score >= 50){
                 System.out.println("D");
-                break;
-
-                default:
-                    System.out.println("F");
-                    break;
+            }
+            else if (score <= 49){
+                System.out.println("F");
+            }
         }
     }
-}
