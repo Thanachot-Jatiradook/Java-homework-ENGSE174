f52a5f19fc874441e862da23d2ad5177e26b3e3d
Thanachot Jatiradook <jatiradook13@gmail.com>
2025-11-27 20:52:40 +0700

    lab20901

--- Lab209/Main.java
+++ Lab209/Main.java
@@ -4,25 +4,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in) ;
 
-        System.out.println("Enter Your Name :     ");
-        String inputName = scanner.nextLine();
-        System.out.println("Enter Your Street :     ");
-        String inputStreet = scanner.nextLine();
-        System.out.println("Enter Your City :     ");
-        String inputCity = scanner.nextLine();
-        System.out.println("Enter Your Zipcode :     ");
-        String inputZipCode = scanner.nextLine();
+        System.out.println("Enter Your Name :     ") ;
+        String inputName = scanner.nextLine() ;
+        System.out.println("Enter Your Street :     ") ;
+        String inputStreet = scanner.nextLine() ;
+        System.out.println("Enter Your City :     ") ;
+        String inputCity = scanner.nextLine() ;
+        System.out.println("Enter Your Zipcode :     ") ;
+        String inputZipCode = scanner.nextLine() ;
 
-        System.out.println("----------result----------");
+        System.out.println("----------result----------") ;
 
-        Address blehAddress = new Address(inputStreet, inputCity, inputZipCode);
+        Address blehAddress = new Address(inputStreet, inputCity, inputZipCode) ;
 
-        Student blehStudent = new Student(inputName, blehAddress);
+        Student blehStudent = new Student(inputName, blehAddress) ;
 
-        blehStudent.displayProfile();
+        blehStudent.displayProfile() ;
 
-        scanner.close();
+        scanner.close() ;
     }
 }
