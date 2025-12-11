7799dce7169fb964aebd646110df67cf2066c792
Thanachot Jatiradook <jatiradook13@gmail.com>
2025-12-11 15:01:04 +0700

    lab401401

--- Lab4014/Main.java
+++ Lab4014/Main.java
@@ -3,41 +3,49 @@ package Lab4014;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter Your Game Name : ");
-        String inputGameName = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in) ;
 
-        System.out.println("-------------------------");
+        System.out.println("Enter Your Game Name : ") ;
+        String inputGameName = scanner.nextLine() ;
 
-        System.out.println("Enter Your Scores 1 :");
-        int inputScore1 = scanner.nextInt();
+        System.out.println("-------------------------") ;
 
-        System.out.println("-------------------------");
+        System.out.println("Enter Your Scores 1 :") ;
+        int inputScore1 = scanner.nextInt() ;
 
-        System.out.println("Enter Your Scores 2 :");
-        int inputScore2 = scanner.nextInt();
+        System.out.println("-------------------------"); 
 
-        System.out.println("-------------------------");
+        System.out.println("Enter Your Scores 2 :") ;
+        int inputScore2 = scanner.nextInt() ;
 
-        System.out.println("Enter Your Scores 3 :");
-        int inputScore3 = scanner.nextInt();
+        System.out.println("-------------------------") ;
 
-        int[] originalScores = {inputScore1, inputScore2, inputScore3};
+        System.out.println("Enter Your Scores 3 :") ;
+        int inputScore3 = scanner.nextInt() ;
 
-        ScoreBoard blehscore1 = new ScoreBoard(inputGameName, originalScores);
+        int[] originalScores = {inputScore1, inputScore2, inputScore3} ;
 
-        ScoreBoard blehscore2 = new ScoreBoard(blehscore1);
+        ScoreBoard blehscore1 = new ScoreBoard(inputGameName, originalScores) ;
 
-        originalScores[0] = 999;
+        ScoreBoard blehscore2 = new ScoreBoard(blehscore1) ;
 
-        System.out.println("----------result----------");
+        System.out.println("-------------------------") ;
 
-        blehscore1.displayScores();
-        blehscore2.displayScores();
+        System.out.println("Enter Your Max Score : ") ;
+        int inputMaxScore = scanner.nextInt() ;
+
+        originalScores[0] = inputMaxScore ;
+
+        System.out.println("----------result----------") ;
+
+        blehscore1.displayScores() ;
+        blehscore2.displayScores() ;
 
         scanner.close();
 
     }
+
 }
